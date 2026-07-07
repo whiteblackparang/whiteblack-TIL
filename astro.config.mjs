389fbd2773c930dd7ebe9b9bf86113fc72118532
Hun <hun5639@naver.com>
2026-07-07 10:39:30 +0900

    feat: GitHub Pages 배포 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://whiteblackparang.github.io',
+	base: '/whiteblack-TIL',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
